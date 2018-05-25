8a2f252fe44415b5bb610e7d4985f36d75a34693
Erick Watson <fredmackay@hotmail.com>
2018-05-25 15:26:46 +0930

    Added Matrices Tutorial

---
+++ Matrices - Tutorial/LibMatrix.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "LibMatrix.h"
+
+
+Matrix2::Matrix2()
+{
+}
+
+
+Matrix2::~Matrix2()
+{
+}
---
+++ Matrices - Tutorial/LibMatrix.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+
+
+class Matrix2
+{
+public:
+	Matrix2();
+	~Matrix2();
+};
+
+class Matrix3 {
+public:
+	Vector3 xAxis;
+	Vector3 yAxis;
+	Vector3 zAxis;
+};
+
+class Matrix4 {
+public:
+
+};
